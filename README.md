568aa728384523337c791b6c60b7e565b546a8c0
crossjs <liwenfu@crossjs.com>
2015-07-08 15:57:54 +0800

    update

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # nd-list
 
-[![spm version](http://spmjs.io/badge/nd-list)](http://spmjs.io/package/nd-list)
+[![spm version](http://spm.crossjs.com/badge/nd-list)](http://spm.crossjs.com/package/nd-list)
 
 > repeat request list
 
@@ -15,34 +15,4 @@ $ spm install nd-list --save
 ```js
 var List = require('nd-list');
 // use List
-```
-## 开发
-
-### 本地 Web 服务
-
-```bash
-grunt
-```
-
-浏览器中访问 http://127.0.0.1:8851
-
-### 生成/查看 API 文档
-
-```bash
-grunt doc
-grunt
-```
-
-浏览器中访问 http://127.0.0.1:8851/doc
-
-### 代码检查与单元测试
-
-```bash
-grunt test
-```
-
-### 发布组件到 SPM 源
-
-```bash
-grunt publish
 ```
--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 /**
- * @module: nd-list
- * @author: lzhengms <lzhengms@gmail.com> - 2015-05-25 14:34:01
+ * @module List
+ * @author lzhengms <lzhengms@gmail.com>
  */
 
 'use strict';
@@ -8,109 +8,129 @@
 var $ = require('jquery');
 
 var Base = require('nd-base');
-var Alert = require('nd-alert');
+var debug = require('nd-debug');
 
-var List  = Base.extend({
+var List = Base.extend({
   attrs: {
     proxy: null,
     // 0: mysql or 1: mongodb
     mode: 0,
-    title:'',
+    title: '',
     params: {
       $count: true,
       $limit: 100
     },
     data: null
   },
-  initialize: function (config) {
+
+  initialize: function(config) {
     List.superclass.initialize.call(this, config);
 
     var proxy = this.get('proxy');
+
     if (!proxy) {
-      console.error('请设置数据源（proxy）');
+      debug.error('请设置数据源（proxy）');
     } else {
-      this.LIST = proxy['LIST'].bind(proxy);
+      ['LIST', 'GET', 'PUT', 'PATCH', 'POST', 'DELETE']
+      .forEach(function(method) {
+        proxy[method] && (this[method] = proxy[method].bind(proxy));
+      }, this);
     }
 
-    var params;
-
-    switch (this.get('mode')) {
-      case 2:
-        params = {};
-        break;
-      case 1:
-        params = {
-          size: 10,
-          page: 0
-        };
-        break;
-      default:
-        params = {
-          $limit: 10,
-          $offset: 0
-        };
-    }
-
-    this.set('params', $.extend(params, this.get('params')));
-
+    this.set('params', $.extend((function(mode) {
+      switch (mode) {
+        // case 2:
+        //   return {};
+        case 1:
+          return {
+            $count: true,
+            size: 10,
+            page: 0
+          };
+        default:
+          return {
+            $count: true,
+            $limit: 10,
+            $offset: 0
+          };
+      }
+    })(this.get('mode')), this.get('params')));
 
     // 取列表
     this.getList();
-
   },
-  getList: function (options) {
+
+  getList: function(options) {
     var that = this;
+
     if (options && options.data) {
       this.set('params', options.data);
     } else {
       options = {};
     }
+
     var params = options.data = $.extend({}, this.get('params'));
 
-    Object.keys(params).forEach(function (key) {
+    Object.keys(params).forEach(function(key) {
       if (params[key] === '') {
         delete params[key];
       }
     });
 
-    this.LIST(options).done(function (data) {
-      if(!data.count && data.items.length){
+    this.LIST(options).done(function(data) {
+      if (!data.count && data.items.length) {
         //没有分页的情况，没有返回data.count
-        data.count=data.items.length;
+        data.count = data.items.length;
       }
+
       if (data.count && data.items.length) {
         that.set('data', data.items);
-        var page = params.$offset / params.$limit;
-        var nowTotal = that.get('mode') ? ++params.page * params.size : ++page * params.$limit;
+
+        var nowTotal = (function(mode) {
+          switch (mode) {
+            // case 2:
+            //   return {};
+            case 1:
+              return ++params.page * params.size;
+            default:
+              return params.$offset + params.$limit;
+          }
+        })(that.get('mode'));
+
         if (data.count > nowTotal) {
           that.getList({
-            data: that.get('mode') ? {
-              page: params.page
-            } : {
-              $offset: page * params.$limit
-            }
+            data: (function(mode) {
+              switch (mode) {
+                // case 2:
+                //   return {};
+                case 1:
+                  return {
+                    page: Math.ceil(data.count / params.size) - 1
+                  };
+                default:
+                  return {
+                    $offset: (Math.ceil(data.count / params.$limit) - 1) * params.$limit
+                  };
+              }
+            })(that.get('mode'))
           });
         } else {
           that.trigger('loaded', that.get('list'));
         }
       } else {
         that.trigger('drain');
-        //Alert.show('没有'+that.get('title')+'数据,请先创建!');
       }
 
-    }).fail(function (error) {
-      Alert.show(error);
+    }).fail(function(error) {
+      debug.error(error);
     });
   },
 
-  _onChangeData: function (data) {
-    var doneData = this.get('list') || [];
-    doneData = doneData.concat(data);
-    this.set('list', doneData);
+  _onChangeData: function(data) {
+    this.set('list', (this.get('list') || []).concat(data));
   }
 
 });
 
 
-module.exports=List;
-
+module.exports = List;
